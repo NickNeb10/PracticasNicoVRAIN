9558314a17caf442953362dc44c7d20d37d97133
Nicolás Nebot <niconebot10@gmail.com>
2025-02-18 11:30:01 +0100

    Modificacion actualizacion registros energéticos

--- Actualizaciones/actualizacion_registros.py
+++ Actualizaciones/actualizacion_registros.py
@@ -8,7 +8,7 @@ import hashlib
 from datetime import datetime
 
 url = 'https://dadesobertes.gva.es/dataset/a8a4b590-772d-4cba-a15c-fe6eff346431/resource/4e454fb5-1ba1-4ccd-8191-cc9b3700e47b/download/certificacion-energetica-de-edificios-en-valencia.csv'
-folder_path = '/home/nnebot/PracticasNicoVRAIN/Datos/RegistrosEnergeticos'
+folder_path = '/home/gti/PracticasNicoVRAIN/Datos/RegistrosEnergeticos'
 
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
@@ -24,18 +24,25 @@ def get_file_hash(file_path):
 response = requests.get(url)
 
 if response.status_code == 200:
-    current_date = datetime.now().strftime('%d-%m-%Y')
-    file_name = f'registros_energeticos_{current_date}.csv'
-    file_path = os.path.join(folder_path, file_name)
-    
     new_file_hash = hashlib.md5(response.content).hexdigest()
-    existing_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
     
-    if file_name not in existing_files:
+    # Buscar en todos los ficheros existentes si alguno tiene el mismo hash
+    existing_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+    file_exists_same = False
+    for existing_file in existing_files:
+        existing_file_path = os.path.join(folder_path, existing_file)
+        if get_file_hash(existing_file_path) == new_file_hash:
+            file_exists_same = True
+            break
+
+    if file_exists_same:
+        print("El fichero descargado es igual a uno existente. No se descarga un nuevo fichero.")
+    else:
+        current_date = datetime.now().strftime('%d-%m-%Y')
+        file_name = f'registros_energeticos_{current_date}.csv'
+        file_path = os.path.join(folder_path, file_name)
         with open(file_path, 'wb') as f:
             f.write(response.content)
         print(f"El archivo ha sido guardado como {file_name}.")
-    else:
-        print(f"El archivo {file_name} ya existe.")
 else:
     print(f"Error al descargar el archivo: {response.status_code}")
