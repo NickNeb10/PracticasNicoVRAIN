fb49dfa38cf9436c6cbe9f3f56f22b5e62122d8b
Nicolás Nebot <niconebot10@gmail.com>
2025-02-18 11:48:52 +0100

    Nueva modificacion registros

--- Actualizaciones/actualizacion_registros.py
+++ Actualizaciones/actualizacion_registros.py
@@ -14,30 +14,37 @@ if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
 def get_file_hash(file_path):
-    """Calcula el hash MD5 de un archivo"""
     try:
         with open(file_path, 'rb') as f:
-            hasher = hashlib.md5()
-            while chunk := f.read(8192):  
-                hasher.update(chunk)
-        return hasher.hexdigest()
+            file_hash = hashlib.md5(f.read()).hexdigest()
+        return file_hash
     except FileNotFoundError:
         return None
 
 response = requests.get(url)
 if response.status_code == 200:
     new_file_hash = hashlib.md5(response.content).hexdigest()
+    
     existing_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.csv')]
-    existing_hashes = {get_file_hash(f) for f in existing_files if get_file_hash(f)}
-
-    if new_file_hash in existing_hashes:
+    existing_hashes = {}
+    for existing_file in existing_files:
+        file_hash = get_file_hash(existing_file)
+        if file_hash:
+            existing_hashes[existing_file] = file_hash
+            print(f"Archivo existente: {existing_file}, Hash: {file_hash}")
+    
+    if new_file_hash in existing_hashes.values():
         print("El fichero descargado es igual a uno existente. No se descarga un nuevo fichero.")
     else:
         current_date = datetime.now().strftime('%d-%m-%Y')
         file_name = f'registros_energeticos_{current_date}.csv'
         file_path = os.path.join(folder_path, file_name)
-        with open(file_path, 'wb') as f:
-            f.write(response.content)
-        print(f"El archivo ha sido guardado como {file_name}.")
+
+        if os.path.exists(file_path):
+            print(f"El archivo {file_name} ya existe en el directorio.")
+        else:
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
+            print(f"El archivo ha sido guardado como {file_name}.")
 else:
     print(f"Error al descargar el archivo: {response.status_code}")
